3624083272d02f26d4d99155eb61ba2867b6803e
Jun <shj0213@gmail.com>
2024-09-16 10:21:08 -0400

    First commit

---
+++ gender.py
@@ -0,0 +1,74 @@
+import os
+import cv2
+import numpy as np
+
+main = '/Users/hunjunsin/Desktop/python/Kaggle/genderclassification/gender_dataset'
+
+men = os.path.join(main,'men')
+women = os.path.join(main, 'women')
+
+def preprocess(directory, label):
+    images = []
+    labels = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg"):
+            img_path = os.path.join(directory, filename)
+            try:
+                img = cv2.imread(img_path)
+                if img is None:
+                    print(f"Unable to read image {img_path}")
+                    continue
+                img = cv2.resize(img, (128, 128))
+                images.append(img)
+                labels.append(label)
+            except Exception as e:
+                print(f"Error processing file {img_path}: {e}")
+    return images, labels
+
+mimages, mlabels = preprocess(men, 0)
+woimages, wolabels = preprocess(women, 1)
+
+from tensorflow.keras.utils import to_categorical
+
+images = np.array(mimages + woimages)
+labels = np.array(mlabels + wolabels)
+
+images = images / 255.0
+labels = to_categorical(labels, num_classes=2)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(128, 128, 3)))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(2, 2))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dense(2, activation='sigmoid'))
+
+model.summary()
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=15, validation_data=(X_test, y_test), batch_size=32)
---
+++ genderclassification/gender.py
@@ -0,0 +1,8 @@
+import os
+import cv2
+import numpy as np
+
+main = '/kaggle/input/human-images-dataset-men-and-women/gender_dataset'
+
+men = os.path.join(main,'men')
+women = os.path.join(main, 'women')
