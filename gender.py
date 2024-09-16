504c4e9d239b879e6a3bd385429d9c64e3ff3ef7
Jun <shj0213@gmail.com>
2024-09-16 10:32:29 -0400

    finish

--- gender.py
+++ gender.py
@@ -72,3 +72,21 @@ model.summary()
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 history = model.fit(X_train, y_train, epochs=15, validation_data=(X_test, y_test), batch_size=32)
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+plt.figure()
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(loc='upper right')
+plt.show()
